4d2b6773efd796d7be6aeb56a6c7e10352ef02d0
Richard Guin <rwguin@gmail.com>
2020-12-08 01:12:20 -0500

    2020.8 complete

---
+++ AdventOfCode2020/Day8/InstructionHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020.Day8
+{
+    class InstructionHandler
+    {
+        private string[] Instructions { get; set; }
+        public long Accumulator { get; private set; }
+        private int InstPtr { get; set; }
+        private List<int> PrevInst { get; set; }
+
+        public InstructionHandler(string[] instructions)
+        {
+            Instructions = instructions;
+            Accumulator = 0;
+            InstPtr = 0;
+            PrevInst = new List<int>();
+        }
+
+        public void Run()
+        {
+            while (InstPtr < Instructions.Length)
+            {
+                if (PrevInst.Contains(InstPtr))
+                    break;
+
+                PrevInst.Add(InstPtr);
+                var parts = Instructions[InstPtr].Split(' ');
+                switch (parts[0])
+                {
+                    case "acc":
+                        Accumulate(int.Parse(parts[1]));
+                        InstPtr++;
+                        break;
+                    case "jmp":
+                        Jump(int.Parse(parts[1]));
+                        break;
+                    case "nop":
+                        InstPtr++;
+                        break;
+                    default:
+                        throw new ArgumentException($"Unknown instruction found on line {InstPtr}: {parts[0]}");
+                }
+            }
+        }
+
+        public bool RanToCompletion => InstPtr >= Instructions.Length;
+
+        private void Accumulate(int val) => Accumulator += val;
+
+        private void Jump(int val) => InstPtr += val;
+    }
+}
---
+++ AdventOfCode2020/Day8/Main.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Day8
+{
+    class Main : BaseClass
+    {
+        public new async Task<string> Part1()
+        {
+            var input = await ReadInput();
+            var handler = new InstructionHandler(input);
+            handler.Run();
+            return handler.Accumulator.ToString();
+        }
+
+        public new async Task<string> Part2()
+        {
+            var input = await ReadInput();
+            var linesToSwitch = new List<int>();
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[i].StartsWith("jmp") || input[i].StartsWith("nop"))
+                    linesToSwitch.Add(i);
+            }
+
+            InstructionHandler handler = null;
+
+            foreach (var line in linesToSwitch)
+            {
+                var newInput = (string[])input.Clone();
+                if (newInput[line].StartsWith("jmp"))
+                    newInput[line] = newInput[line].Replace("jmp", "nop");
+                else
+                    newInput[line] = newInput[line].Replace("nop", "jmp");
+
+                handler = new InstructionHandler(newInput);
+                handler.Run();
+                if (handler.RanToCompletion)
+                    break;
+            }
+
+            if (!handler.RanToCompletion)
+                throw new Exception("None of the switches worked!");
+
+            return handler?.Accumulator.ToString();
+        }
+
+        private string[] GetSample()
+        {
+            return @"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6".Split(Environment.NewLine);
+        }
+    }
+}
