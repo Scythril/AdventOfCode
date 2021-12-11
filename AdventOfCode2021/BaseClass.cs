65ed08ed123125ef1c503f37ea15694a45e90de4
Richard Guin <rwguin@gmail.com>
2021-12-10 23:11:43 -0500

    2021.1 complete

---
+++ AdventOfCode2021/BaseClass.cs
@@ -0,0 +1,68 @@
+﻿namespace AdventOfCode2021
+{
+    abstract class BaseClass
+    {
+        public string DayName => GetType().Namespace?.Split('.').Last() ?? String.Empty;
+        public bool Debug = true;
+
+        public async Task<string> Part1()
+        {
+            return string.Empty;
+        }
+
+        public async Task<string> Part2()
+        {
+            return string.Empty;
+        }
+
+        public async Task<string[]> ReadInput()
+        {
+            try
+            {
+                return await File.ReadAllLinesAsync(Path.Combine(DayName, "input.txt"));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            return Array.Empty<string>();
+        }
+
+        public async Task<string> ReadInputAsString()
+        {
+            try
+            {
+                return await File.ReadAllTextAsync(Path.Combine(DayName, "input.txt"));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            return String.Empty;
+        }
+
+        public void LogMessage(string message)
+        {
+            if (Debug)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+        }
+
+        public void LogError(string message)
+        {
+            if (Debug)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ AdventOfCode2021/Day1/Main.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode2021.Day1
+{
+    internal class Main : BaseClass
+    {
+        public new async Task<string> Part1()
+        {
+            var input = (await ReadInput()).Select(x => int.Parse(x));
+            var increases = 0;
+            var lastDepth = int.MaxValue;
+            foreach (var depth in input)
+            {
+                if (depth > lastDepth)
+                    increases++;
+                lastDepth = depth;
+            }
+
+            return increases.ToString();
+        }
+
+        public new async Task<string> Part2()
+        {
+            var input = (await ReadInput()).Select(x => int.Parse(x)).ToArray();
+            //var input = GetSample().Select(x => int.Parse(x)).ToArray();
+            var increases = 0;
+            var lastSum = int.MaxValue;
+            for (var i = 0; i < input.Length - 2; i++)
+            {
+                var sum = input[i] + input[i + 1] + input[i + 2];
+                if (sum > lastSum)
+                    increases++;
+                lastSum = sum;
+            }
+
+            return increases.ToString();
+        }
+
+        private string[] GetSample()
+        {
+            var sample = @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+            return sample.Split(Environment.NewLine);
+        }  
+    }
+}
